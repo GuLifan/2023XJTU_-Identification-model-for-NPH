642d6ae6d773d7680f5eaab88a02fa5574427695
orion <net4orion@163.com>
2023-11-03 20:18:53 +0800

    Add: param bilinear

--- fine-tune.py
+++ fine-tune.py
@@ -1,6 +1,6 @@
 # 可调节的参数有:
 # 训练层
-#   epochs, batch_size, lr初始值
+#   bilinear, epochs, batch_size, lr初始值
 # 数据处理层
 #   scale
 # 预测结果
@@ -32,55 +32,57 @@ def fine_tune():
     # perflog
     train_perflog = []
     best_record = {"avg_accuracy": 0}
-    for epochs in range(50, 210, 10):
-        for batch_size in range(1, 51):
-            for lr in np.arange(0.01, 0.11, 0.01):
-                for scale in range(1, 21):
-                    net = UNet(n_channels=1, n_classes=1, bilinear=True)
-                    net.to(device)
-                    pprint(
-                        f"Train params: epochs={epochs}, batch_size={batch_size}, lr={lr}, scale={scale}"
-                    )
-                    try:
-                        loss = train_net(
-                            net,
-                            device,
-                            data_path,
-                            output_path,
-                            epochs,
-                            batch_size,
-                            lr,
-                            scale,
+    for bilinear in [True, False]:
+        for epochs in range(50, 210, 10):
+            for batch_size in range(1, 51):
+                for lr in np.arange(0.01, 0.11, 0.01):
+                    for scale in range(1, 21):
+                        net = UNet(n_channels=1, n_classes=1, bilinear=bilinear)
+                        net.to(device)
+                        pprint(
+                            f"Train params: bilinear={bilinear}, epochs={epochs}, batch_size={batch_size}, lr={lr}, scale={scale}"
                         )
-                    except:
-                        pprint("Train failed")
-                        continue
-                    for threshold in np.arange(0.1, 1.1, 0.1):
-                        pprint(f"|----Predict param: threshold={threshold}")
                         try:
-                            predict(net, device, sources_path, threshold)
-                            avg_accuracy = eval(sources_path, labels_path)
-                            pprint(
-                                f"|    |----Result: loss={loss}, accuracy={avg_accuracy}"
+                            loss = train_net(
+                                net,
+                                device,
+                                data_path,
+                                output_path,
+                                epochs,
+                                batch_size,
+                                lr,
+                                scale,
                             )
                         except:
-                            pprint("|    |----Train failed")
-                            loss = 1
-                            avg_accuracy = 0
+                            pprint("Train failed")
+                            continue
+                        for threshold in np.arange(0.1, 1.1, 0.1):
+                            pprint(f"|----Predict param: threshold={threshold}")
+                            try:
+                                predict(net, device, sources_path, threshold)
+                                avg_accuracy = eval(sources_path, labels_path)
+                                pprint(
+                                    f"|    |----Result: loss={loss}, accuracy={avg_accuracy}"
+                                )
+                            except:
+                                pprint("|    |----Train failed")
+                                loss = 1
+                                avg_accuracy = 0
 
-                        record = {
-                            "epochs": epochs,
-                            "batch_size": batch_size,
-                            "lr": lr,
-                            "scale": scale,
-                            "threshold": threshold,
-                            "loss": loss,
-                            "avg_accuracy": avg_accuracy,
-                        }
-                        if record["avg_accuracy"] > best_record["avg_accuracy"]:
-                            best_record = record
-                        train_perflog.append(record)
-                    # del net
+                            record = {
+                                "bilinear": bilinear,
+                                "epochs": epochs,
+                                "batch_size": batch_size,
+                                "lr": lr,
+                                "scale": scale,
+                                "threshold": threshold,
+                                "loss": loss,
+                                "avg_accuracy": avg_accuracy,
+                            }
+                            if record["avg_accuracy"] > best_record["avg_accuracy"]:
+                                best_record = record
+                            train_perflog.append(record)
+                        # del net
 
     json.dump(
         train_perflog, open("train_perflog.json", "w"), indent=4, ensure_ascii=False
